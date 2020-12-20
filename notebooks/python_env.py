912053134451ccb7b2b76c7a27dc92f63523df91
Bryan Palmer <palmer.bryan@gmail.com>
2020-12-20 13:01:53 +1100

    refined python_env()

--- notebooks/python_env.py
+++ notebooks/python_env.py
@@ -6,24 +6,37 @@ import sys
 import platform
 import psutil
 
-import numpy as np
-import pandas as pd
-import matplotlib as mpl
-
-# --- utility functions
+## --- utility functions
 
 def python_env():
+    """This function prints the key system architecture and lists
+       the headline imported modules with version numnbers.
+       There are no arguments for this function.
+       The function returns None."""
+    
     N = 50
-    print('-'*N)
+    
+    # system architecture
+    print('System:')
+    print('='*N)
     print(f"System:     {platform.system()}")
     print(f"Release:    {platform.release()}")
     print(f"Machine:    {platform.machine()}")
     print(f"Processor:  {platform.processor()}")
     print(f"RAM:        {round(psutil.virtual_memory().total / 1024**3)}GB")
-    print('-'*N)
     print(f"Python:     {platform.python_version()}")
-    print(f"Psutil:     {psutil.__version__}")
-    print(f"Pandas:     {pd.__version__}")
-    print(f"Numpy:      {np.__version__}")
-    print(f"Matplotlib: {mpl.__version__}")
+
+    # headline modules with version numbers
+    print('\nModules:')
     print('-'*N)
+    modules = {}
+    for m in sys.modules.keys():
+        if m.startswith('_') or '.' in m:
+            continue
+        if hasattr(sys.modules[m], '__version__'):
+            modules[m] = sys.modules[m] 
+
+    for m in sorted(modules.keys(), key=str.casefold):
+        print(f"{m}: {modules[m].__version__}")
+
+    return(None)
