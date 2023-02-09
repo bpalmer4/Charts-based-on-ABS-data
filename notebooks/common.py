49f53126c6b64e124a46cb05f6009423fa6156ea
bpalmer4 <palmer.bryan@gmail.com>
2023-02-09 12:12:10 +1100

    Made Central Bank Policy Rates a separate notebook

--- notebooks/common.py
+++ notebooks/common.py
@@ -1,18 +1,18 @@
 # common.py
+import pandas as pd
 
 import re
 import requests
-from typing import Optional
+from typing import Optional, Tuple
 from pathlib import Path
 
-
 _bad_cache_pattern = re.compile('[~"#%&*:<>?/\\{|}\.]+')
-def cachefy_name(name: str) -> str:
+def cachefy_name(name:str) -> str:
     new_name = re.sub(_bad_cache_pattern, '_', name)
     return new_name
 
 
-def request_get(url: str) -> Optional[bytes]:
+def request_get(url:str) -> Optional[bytes]:
     gotten = requests.get(url, allow_redirects=True)
     code = gotten.status_code
     if code != 200:
@@ -25,3 +25,31 @@ def request_get(url: str) -> Optional[bytes]:
 def save_to_cache(path:Path, contents:bytes):
     path.open(mode='w', buffering=-1, encoding=None, errors=None, newline=None)
     path.write_bytes(contents)
+
+    
+# --- get Bank of International Settlements data
+def get_bis_cbpr() -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Get the Central Bank Policy Rate data from the Bank
+       of International Settlements (BIS).
+       Returns a Tuple of two pandas Dataframes. The first 
+       DataFrame contains meta-data. The scond contains the 
+       actual data."""
+
+    url = "https://www.bis.org/statistics/full_cbpol_d_csv_row.zip"
+    bis = pd.read_csv(url, low_memory=False, header=None)
+
+    META_ROWS = 9
+    bis_meta = bis[:META_ROWS].copy()
+    bis_meta = bis_meta.set_index(0).T
+    #display(bis_meta.head())
+
+    bis_data = bis[META_ROWS:].copy()
+    bis_data = bis_data.set_index(0)
+    bis_data.index = pd.PeriodIndex(bis_data.index, freq='D')
+    names = bis_meta['Reference area'].str[3:]
+    bis_data.columns = names
+    bis_data = bis_data.astype(float).ffill()
+    #display(bis_data.tail())
+    
+    return (bis_meta, bis_data)
+
