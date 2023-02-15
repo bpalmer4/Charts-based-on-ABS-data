5f08e52c86388997cffeb1ce613298fca6e7d2e6
bpalmer4 <palmer.bryan@gmail.com>
2023-02-15 19:38:24 +1100

    Upgraded to (1) Python 3.11 (2) Jupyter Lab

---
+++ notebooks/mamba-install-modules.sh
@@ -0,0 +1,12 @@
+
+mamba install pandas matplotlib numpy
+mamba install matplotlib seaborn 
+mamba install statsmodels scipy
+mamba install jupyter jupyterlab
+mamba install pyarrow parquet
+mamba install watermark
+mamba install lxml bs4
+mamba install openpyxl xlrd
+mamba install pandasdmix requests_cache
+mamba install arrow
+
