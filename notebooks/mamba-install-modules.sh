ccfbf934b394cec93d20e4a4a0a5bb30a7dceb88
bpalmer4 <palmer.bryan@gmail.com>
2023-02-25 23:15:02 +1100

    Minor updates

--- notebooks/mamba-install-modules.sh
+++ notebooks/mamba-install-modules.sh
@@ -1,11 +1,13 @@
 mamba install pandas numpy
 mamba install matplotlib seaborn 
-mamba install statsmodels scipy
+mamba install statsmodels scipy scikit-learn nltk
 mamba install jupyter jupyterlab nodejs
-mamba install pyarrow parquet
+mamba install pyarrow parquet fastparquet h5py
 mamba install watermark
 mamba install lxml bs4
 mamba install openpyxl xlrd
 mamba install pandasdmix requests_cache
 mamba install arrow
-mamba install pylint flake8 black
+mamba install pylint flake8 black nbqa
+mamba install polars
+
