b78f9ef518485013815e1797c20073854b2f9d3b
bpalmer4 <palmer.bryan@gmail.com>
2023-10-14 12:59:11 +1100

    updates

--- notebooks/mamba-install-modules.sh
+++ notebooks/mamba-install-modules.sh
@@ -1,9 +1,12 @@
+mamba create -n p312 python=3.12 -c conda-forge
+mamba activate p312
+
 mamba install pandas numpy
 mamba install matplotlib seaborn 
 mamba install statsmodels scipy scikit-learn nltk
 mamba install jupyter jupyterlab nodejs watermark
 mamba install jupyterlab-spellchecker jupyterlab_code_formatter
-mamba install pyarrow parquet fastparquet h5py
+mamba install pyarrow fastparquet h5py
 mamba install lxml bs4 openpyxl xlrd requests_cache
 mamba install arrow polars pandasdmx 
 mamba install pylint flake8 black nbqa ruff isort
