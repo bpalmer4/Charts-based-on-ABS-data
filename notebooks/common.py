96493f48888a0cece642a39e69c5366a95a32586
bpalmer4 <palmer.bryan@gmail.com>
2023-12-23 21:10:42 +1100

    minor updates

--- notebooks/common.py
+++ notebooks/common.py
@@ -1,9 +1,10 @@
 """ Common data capture functions."""
 
 # --- imports
-import hashlib
+from hashlib import md5 
 import re
 from datetime import datetime, timezone
+from os import utime
 from pathlib import Path
 
 import pandas as pd
@@ -35,15 +36,20 @@ def request_get(url: str) -> bytes:
 
 def save_to_cache(file: Path, contents: bytes) -> None:
     """Save bytes to the file-system."""
-
     if file.exists():
         print("Removing old cache file.")
         file.unlink()
-
     file.open(mode="w", buffering=-1, encoding=None, errors=None, newline=None)
     print(f"Saving to cache: {file}")
     file.write_bytes(contents)
 
+
+def retrieve_from_cache(file: Path) -> bytes:
+    """Retrieve bytes from file-system."""
+    if not file.exists() or not file.is_file():
+        raise CacheError("Cached file not available?")
+    return file.read_bytes()
+    
 
 def get_file(url: str, cache_dir: Path) -> bytes:
     """Get file from URL or local file-system cache, depending on freshness."""
@@ -51,25 +57,20 @@ def get_file(url: str, cache_dir: Path) -> bytes:
     def get_fpath() -> Path:
         """Convert URL string into a cache file name,
         then return as a Path object."""
-
-        bad_cache_pattern = r'[~"#%&*:<>?\\{|}]+'
-        raw_name = (
-            f"{hashlib.md5(url.encode('utf-8')).hexdigest()}--{url.split(r'/')[-1]}"
-        )
-        file_name = re.sub(bad_cache_pattern, "", raw_name)
-        return Path(cache_dir / f"{file_name}")
+        bad_cache_pattern = r'[~"#%&*:<>?\\{|}]+'  # remove these chars from name
+        hash_name = md5(url.encode('utf-8')).hexdigest()
+        tail_name = url.split('/')[-1]
+        file_name = re.sub(bad_cache_pattern, "", f"{hash_name}--{tail_name}")
+        return Path(cache_dir / file_name)
 
     # sanity checks
     if not cache_dir.is_dir():
         raise ValueError("Cache path is not a directory")
 
     # get URL modification time in UTC
-    response = requests.head(
-        url, allow_redirects=True, timeout=20
-    )  # only get the header
+    response = requests.head(url, allow_redirects=True, timeout=20)
     check_response(url, response)
     source_mtime = pd.to_datetime(response.headers["Last-Modified"], utc=True)
-    print(f"Source modification date: {source_mtime}")
 
     # get cache modification time in UTC
     target_mtime: datetime | None = None
@@ -80,24 +81,21 @@ def get_file(url: str, cache_dir: Path) -> bytes:
         )
 
     # get and save URL source data
-    one_day_old = pd.Timestamp.utcnow() - pd.DateOffset(days=1)
     if (
         target_mtime is None  # cache is empty
         or source_mtime > target_mtime  # URL is fresher than cache
-        or one_day_old > target_mtime  # prophylaxis: our cache might just be stale
     ):
         print("About to download and cache the latest data.")
         url_bytes = request_get(url)  # will raise exception if it fails
         save_to_cache(file_path, url_bytes)
+        # - change file mod time to reflect mtime at URL
+        unixtime = source_mtime.value / 1_000_000_000  # convert to seconds
+        utime(file_path, (unixtime, unixtime))
         return url_bytes
 
     # return the data that has been cached previously
-    file_path = get_fpath()
     print(f"Retrieving data from the cache file: {file_path}")
-    if not file_path.exists() or not file_path.is_file():
-        raise CacheError("Cached file not available?")
-    file_bytes = file_path.read_bytes()
-    return file_bytes
+    return retrieve_from_cache(file_path)
 
 
 # --- preliminary testing:
