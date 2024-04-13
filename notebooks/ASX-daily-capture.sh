495de8e3825759a2828eda26748f2538706413be
bpalmer4 <palmer.bryan@gmail.com>
2024-04-13 14:41:41 +1000

    Added ASX rate tracker

---
+++ notebooks/ASX-daily-capture.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+# crontab: 01 20 * * 1-5 /Users/bryanpalmer/ABS/notebooks/ASX-daily-capture.sh
+# note: * * * * * ==> minute hour day-of-month month day-of-week
+
+# set-up parameters
+home=/Users/bryanpalmer
+working=ABS/notebooks
+runrun=ASX-daily-data-capture.py
+mmenv=312
+
+# move to the home directory
+cd $home
+
+# activate the micromamba environment
+micromamba activate $mmenv
+
+# move to the working directory
+cd $working
+
+# initiate the data capture
+$home/micromamba/envs/$mmenv/bin/python ./$runrun >>./LOGS/asx-log.log 2>>./LOGS/asx-err.log
+
+# update git
+#git commit "../betting-data/sportsbet-2025-election-winner.csv" -m "data update"
+#git push
---
+++ notebooks/ASX-daily-data-capture.py
@@ -0,0 +1,52 @@
+"""Daily ASX rate scraper - based on Matt Cowgil's R scraper at 
+   https://github.com/MattCowgill/cash-rate-scraper.git"""
+
+# imports
+from pathlib import Path
+from typing import Final
+
+import json
+import pandas as pd
+import common
+
+# nameing conventions
+CASH_RATE: Final = 'cash_rate'
+SCRAPE_DATE: Final = 'scrape_date'
+DATE = 'date'
+FILE_STEM = 'scraped_cash_rate_'
+
+
+def get_asx_data() -> pd.DataFrame:
+    """Capture the latest ASX rate tracker data from the ASX website 
+    and return it as a pandas DataFrame."""
+    
+    url = (
+        "https://asx.api.markitdigital.com/asx-research/1.0/derivatives/"
+        "interest-rate/IB/futures?days=1&height=179&width=179"       
+    )
+    raw_jason = common.request_get(url)
+    df = pd.DataFrame(json.loads(raw_jason)['data']['items'])
+    df[CASH_RATE] = (100 - df.pricePreviousSettlement).round(3)
+    df[SCRAPE_DATE] = df.loc[df['dateLastTrade'].notna(), 'dateLastTrade'].max()
+    df.index = pd.PeriodIndex(df['dateExpiry'], freq='M')
+    df.index.name = DATE  # consistency with Matt Cowgil's R scraper
+    return df[[CASH_RATE, SCRAPE_DATE]]
+
+
+def save_asx_data(df: pd.DataFrame) -> None:
+    """Save the ASX rate tracker data to a CSV file."""
+
+    directory = "./ASX_DAILY_DATA/"
+    Path(directory).mkdir(parents=True, exist_ok=True)
+    filename = f"{directory}{FILE_STEM}{df[SCRAPE_DATE].max()}.csv"
+    get_asx_data().to_csv(path_or_buf=filename)
+
+
+def main() -> None:
+    """The main function to capture and save the ASX rate tracker data."""
+
+    df = get_asx_data()
+    save_asx_data(df)
+
+
+main()
