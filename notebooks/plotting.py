d39b834b71e3fcffc9621fc0f6089037a326abb9
bpalmer4 <palmer.bryan@gmail.com>
2024-08-01 16:33:24 +1000

    Removed recalibrate() - now in readabs package

--- notebooks/plotting.py
+++ notebooks/plotting.py
@@ -6,8 +6,6 @@
 # --- imports
 # system imports
 import re
-import sys
-from operator import mul, truediv
 from pathlib import Path
 from typing import Any, Final, cast, TypeVar
 
@@ -40,7 +38,7 @@ COLOR_GREEN: Final[str] = "mediumseagreen"
 
 NARROW_WIDTH: Final[float] = 0.75
 WIDE_WIDTH: Final[float] = 2.0
-LEGEND_FONTSIZE: Final[str] = "8"
+LEGEND_FONTSIZE: Final[str] = "small"
 LEGEND_SET: Final[dict[str, Any]] = {"loc": "best", "fontsize": LEGEND_FONTSIZE}
 
 
@@ -360,6 +358,7 @@ def finalise_plot(axes, **kwargs) -> None:
 
 # constants
 STARTS, TAGS = "starts", "tags"
+AX = "ax"
 STYLE, WIDTH, COLOR = "style", "width", "color"
 ALPHA, LEGEND, DROPNA = "alpha", "legend", "dropna"
 DRAWSTYLE, MARKER = "drawstyle", "marker"
@@ -408,7 +407,7 @@ def _get_multi_starts(**kwargs) -> tuple[dict[str, list], dict]:
     # Ensure that the tags are not identical ...
     if len(stags[TAGS]) > 1 and stags[TAGS].count(stags[TAGS][0]) == len(stags[TAGS]):
         stags[TAGS] = [
-            e + f"{i:02d}" if i > 0 else e for i, e in enumerate(stags[TAGS])
+            e + f"{i:03d}" if i > 0 else e for i, e in enumerate(stags[TAGS])
         ]
 
     return stags, kwargs
@@ -505,12 +504,16 @@ def line_plot(data: _DataT, **kwargs: Any) -> None:
     - drawstyle - str | list[str] - pandas drawing style
       if False, no legend will be displayed.
     - dropna - bool - whether to delete NAs before plotting
+    - ax - plt.Axes | None - axes to plot on (optional)
     - Remaining arguments as for finalise_plot() [but note, the tag
       argument to finalise_plot cannot be used. Use tags instead.]"""
 
     # sanity checks
-    assert isinstance(data, (Series, DataFrame))
-    assert isinstance(data.index, pd.PeriodIndex)
+    if (not isinstance(data, (Series, DataFrame))
+        or not isinstance(data.index, pd.PeriodIndex)):
+        raise TypeError("The data argument must be a pandas Series or DataFrame with a PeriodIndex")
+    if AX in kwargs and kwargs[AX] is not None and STARTS in kwargs:
+        print("Caution: only one chart can be plotted with the passed axes")
 
     # really we are only plotting DataFrames
     df = DataFrame(data)
@@ -528,7 +531,8 @@ def line_plot(data: _DataT, **kwargs: Any) -> None:
         if start and not isinstance(start, pd.Period):
             start = pd.Period(start, freq=cast(pd.PeriodIndex, df.index).freq)
         recent = df[df.index >= start] if start else df
-        axes = None
+        # note cammot use ax and start/tag in finalise_plot
+        axes: None | plt.Axes = kwargs.pop(AX, None)
         for i, column in enumerate(recent):
             if (recent[column].isna()).all():
                 continue
@@ -549,7 +553,6 @@ def line_plot(data: _DataT, **kwargs: Any) -> None:
             )
 
         if LEGEND in swce and isinstance(swce[LEGEND], dict):
-            # Have pruned here - might be problematic
             kwargs[LEGEND] = swce[LEGEND].copy()
         if axes:
             finalise_plot(axes, tag=tag, **kwargs)
@@ -586,12 +589,6 @@ def get_projection(original: Series, to_period: pd.Period) -> Series:
 
     y_regress = original[original.index <= to_period].copy()
     x_regress = np.arange(len(y_regress))
-    regress_data = DataFrame(
-        {
-            "y": y_regress.values,
-            "x": x_regress,
-        }
-    )
     m, b = np.polyfit(x_regress, y_regress, 1)
 
     x_complete = np.arange(len(original))
@@ -893,152 +890,3 @@ def calc_and_plot_growth(
         from_,
         **kwargs,
     )
-
-
-# --- data recalibration
-
-# private
-_MIN_RECALIBRATE = "number"  # all lower case
-_MAX_RECALIBRATE = "decillion"  # all lower case
-_keywords = {
-    _MIN_RECALIBRATE.title(): 0,
-    "Thousand": 3,
-    "Million": 6,
-    "Billion": 9,
-    "Trillion": 12,
-    "Quadrillion": 15,
-    "Quintillion": 18,
-    "Sextillion": 21,
-    "Septillion": 24,
-    "Octillion": 27,
-    "Nonillion": 30,
-    _MAX_RECALIBRATE.title(): 33,
-}
-_r_keywords = {v: k for k, v in _keywords.items()}
-
-
-# private
-def _find_calibration(units: str) -> str | None:
-    found = None
-    for keyword in _keywords:
-        if keyword in units or keyword.lower() in units:
-            found = keyword
-            break
-    return found
-
-
-# private
-def _can_recalibrate(flat_data: np.ndarray, units: str, verbose: bool = False) -> bool:
-    if not np.issubdtype(flat_data.dtype, np.number):
-        print("recalibrate(): Non numeric input data")
-        return False
-    if np.isnan(flat_data).all():
-        print("recalibrate(): All NaN data.")
-        return False
-    if (np.isinf(flat_data)).any():
-        print("recalibrate(): Includes non-finite data.")
-        return False
-    if _find_calibration(units) is None:
-        if verbose:
-            print("recalibrate(): Units not appropriately " f"calibrated: {units}")
-        return False
-    if np.nanmax(np.abs(flat_data)) == 0:
-        print("recalibrate(): All zero data")
-        return False
-    if flat_data.max() <= 1000 and flat_data.max() >= 1:
-        if verbose:
-            print("recalibrate(): No adjustments needed")
-        return False
-    return True
-
-
-# private
-def _do_recal(flat_data, units, step, operator):
-    calibration = _find_calibration(units)
-    factor = _keywords[calibration]
-    if factor + step not in _r_keywords:
-        print(f"Unexpected factor: {factor + step}")
-        sys.exit(-1)
-    replacement = _r_keywords[factor + step]
-    units = units.replace(calibration, replacement)
-    units = units.replace(calibration.lower(), replacement)
-    flat_data = operator(flat_data, 1000)
-    return flat_data, units
-
-
-# public
-def recalibrate(
-    data: _DataT,
-    units: str,
-    verbose: bool = False,
-) -> tuple[_DataT, str]:
-    """Recalibrate a pandas Series or DataFrame."""
-
-    flat_data = data.to_numpy().flatten()
-
-    # manage the names for some gnarly units
-    possible_units = ("$", "Tonnes")  # there may be more possible units
-    found = False
-    for pu in possible_units:
-        if pu.lower() in units.lower():
-            units = units.lower().replace(pu.lower(), "").strip()
-            if units == "":
-                units = "number"
-            found = True
-            break
-
-    if _can_recalibrate(flat_data, units, verbose):
-        while True:
-            maximum = np.nanmax(np.abs(flat_data))
-            if maximum > 1000:
-                if _MAX_RECALIBRATE in units.lower():
-                    print("recalibrate() is not designed for very big units")
-                    break
-                flat_data, units = _do_recal(flat_data, units, 3, truediv)
-                continue
-            if maximum < 1:
-                if _MIN_RECALIBRATE in units.lower():
-                    print("recalibrate() is not designed for very small units")
-                    break
-                flat_data, units = _do_recal(flat_data, units, -3, mul)
-                continue
-            break
-
-    if found:
-        units = f"{pu} {units}"
-        for n in "numbers", "number":
-            if n in units:
-                units = units.replace(n, "").strip()
-                break
-    units = units.title()
-
-    restore_pandas = DataFrame if len(data.shape) == 2 else Series
-    result = restore_pandas(flat_data.reshape(data.shape))
-    result.index = data.index
-    if len(data.shape) == 2:
-        result.columns = data.columns
-    if len(data.shape) == 1:
-        result.name = data.name
-    return result, units
-
-
-# public
-def recalibrate_series(
-    input_series: Series,
-    units: str,
-    verbose: bool = False,
-) -> tuple[Series, str]:
-    """Retained for compatibility with earlier code.
-    It calls recalibrate()."""
-
-    recal_series, units = recalibrate(input_series, units, verbose)
-    return recal_series, units
-
-
-# public
-def recalibrate_value(value: float, units: str) -> tuple[float, str]:
-    """Recalibrate a floating point value."""
-
-    series = Series([value])
-    output, units = recalibrate(series, units)
-    return output.values[0], units
