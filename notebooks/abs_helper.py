e8d08786c19a0bc758047d8c4141cae29454a7cc
bpalmer4 <palmer.bryan@gmail.com>
2025-01-06 13:10:15 +1100

    Moved function get_gdp() to abs_helper.py

--- notebooks/abs_helper.py
+++ notebooks/abs_helper.py
@@ -7,6 +7,59 @@ import readabs as ra
 from plotting import set_chart_dir, clear_chart_dir
 
 
+# === frequently used data sources
+
+
+def get_gdp(gdp_type="CP", seasonal="SA") -> tuple[pd.Series, str]:
+    """Return the ABS GDP Series (from the key aggregates table).
+    Arguments:
+    - gdp_type: str - The type of the series to return.
+      gdp_types is one of: 'CP' or 'CVM, which stand for
+      'Current price' and 'Chain volume measures'.
+    - seasonal: str - The seasonal adjustment type.
+      seasonal is one of 'SA', 'T', 'O', which stand for
+      'Seasonally Adjusted', 'Trend' and 'Original'.
+    Returns:
+    a tuple comprising the selected GDP series and the units
+    of that series."""
+
+    # validate the arguments
+    did_cvm = "Gross domestic product: Chain volume measures ;"
+    did_cp = "Gross domestic product: Current prices ;"
+    gdp_types = {
+        # gdp-type: data-item-description in the ABS data
+        "CP": did_cp,
+        "Current price": did_cp,
+        "Current prices": did_cp,
+        "CVM": did_cvm,
+        "Volumetric": did_cvm,
+    }
+    seasonals = {
+        "SA": "Seasonally Adjusted",
+        "S": "Seasonally Adjusted",
+        "T": "Trend",
+        "O": "Original",
+    }
+    assert gdp_type in gdp_types, f"Invalid GDP type: {gdp_type}"
+    assert seasonal in seasonals, f"Invalid seasonal adjustment type: {seasonal}"
+
+    # get the series
+    cat = "5206.0"
+    seo = "5206001_Key_Aggregates"
+    (
+        gdp_data,
+        gdp_meta,
+    ) = ra.read_abs_cat(cat, single_excel_only=seo, verbose=False)
+    selector = {
+        gdp_types[gdp_type]: ra.metacol.did,
+        seasonals[seasonal]: ra.metacol.stype,
+    }
+    table, series_id, units = ra.find_abs_id(gdp_meta, selector, verbose=False)
+    gdp = gdp_data[table][series_id]
+
+    return gdp, units
+
+
 # === data retrieval and initialisation
 def get_abs_data(
     cat: str,
