3fa3227400372a1289a259fc1a494d1ac65d0579
Bryan Palmer <palmer.bryan@gmail.com>
2025-07-15 21:08:04 +1000

    Adding SDMX versions

--- notebooks/lint-all.sh
+++ notebooks/lint-all.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-# Apply a set of lint checks (black, mypy, pylint, ruff) to 
-# the calling arguments. Works for Python files and Jupyter 
-# notebooks.
+# Apply a set of lint checks (mypy, ruff) to the calling arguments.i
+# Note ruff looks to rules in ../pyproject.toml 
+# Works for Python files and Jupyter notebooks.
 
 for arg in "$@"
 do
@@ -13,38 +13,21 @@ do
     fi
     echo "Linting \"$arg\" ..."
     if [[ "$arg" == *.ipynb ]]; then
-        echo "which is a Jupyter notebook ..."
-        echo "black ..."
-        nbqa black "$arg"
-        echo "pylint ..."
-        nbqa pylint "$arg"
+        echo "whch is a Jupyter notebook ..."
         echo "ruff ..."
-        nbqa ruff "$arg"
-        echo "\n\nmypy ..."
+        nbqa ruff --fix "$arg"
+        echo "mypy ..."
         nbqa mypy "$arg"
-        echo "\n\nChecking for type and pylint overrides ..."
-        grep "# type: " "$arg"
-        grep "# pylint: " "$arg"
-        grep --regexp="from typing import .*cast" "$arg"
-        grep "cast(" "$arg"
         continue
     fi
     if [[ "$arg" == *.py ]]; then
         echo "which is a Python file ..."
-        echo "black ..."
-        black "$arg"
-        echo "pylint ..."
-        pylint "$arg"
         echo "ruff ..."
-        ruff check "$arg"
+        ruff check --fix"$arg"
+        ruff format "$arg"
         echo "mypy ..."
         mypy "$arg"
-        echo "Checking for type and pylint overrides ..."
-        grep "# type: " "$arg"
-        grep "# pylint: " "$arg"
-        grep --regexp="from typing import .*cast" "$arg"
-        grep "cast(" "$arg"
-       continue
+        continue
     fi
     echo "But file type not supported, skipping ..."
 done
